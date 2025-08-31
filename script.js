6cedada06cbfb3306865256993a9beb0c6aebbe7
emmadumbi <149509955+emmadumbi@users.noreply.github.com>
2025-09-01 00:36:25 +0100

    added some new features

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-"use strict";
 const rockBtn = document.querySelector("#rockBtn");
 const paperBtn = document.querySelector("#paperBtn");
 const resetBtn = document.querySelector("#resetBtn");
@@ -6,15 +5,13 @@ const scissorsBtn = document.querySelector("#scissorsBtn");
 const resultEl = document.querySelector("#resultEl");
 const scoreEl = document.querySelector("#scoreEl");
 const moves = document.querySelector(".moves");
+const autoplayBtn = document.querySelector("#autoplay");
 
 let score = JSON.parse(localStorage.getItem("score")) || {
   wins: 0,
   losses: 0,
   ties: 0,
 };
-
-let result = "";
-let computerMove = "";
 
 scoreEl.innerHTML = `<span>Wins: ${score.wins}</span> -  <span>Loses: ${score.losses}</span> - <span>Ties: ${score.ties}</span>`;
 
@@ -30,8 +27,28 @@ scissorsBtn.addEventListener("click", () => {
   compareMove("scissors");
 });
 
+let isAutoPlaying = false;
+let intervalId;
+
+const autoPlay = () => {
+  if (!isAutoPlaying) {
+    intervalId = setInterval(() => {
+      const playerMove = getComputerMove();
+      compareMove(playerMove)
+    }, 2000);
+    console.log(intervalId)
+    isAutoPlaying = true;
+  } else {
+    clearInterval(intervalId);
+    isAutoPlaying = false;
+  };
+  autoplayBtn.textContent = isAutoPlaying ? "Auto Playing" : "Auto Play";
+};
+ 
+ 
 // function to determine computer's move
-const getComputerMove = function () {
+const getComputerMove = function() {
+  let computerMove = "";
   const randomMove = Math.random();
   if (randomMove >= 0 && randomMove < 1 / 3) {
     computerMove = "rock";
@@ -40,87 +57,53 @@ const getComputerMove = function () {
   } else if (randomMove >= 2 / 3 && randomMove < 1) {
     computerMove = "scissors";
   }
+  return computerMove;
 };
 
 //function to check player move vs computer move
-const compareMove = function (playerMove) {
-  getComputerMove();
-
+const compareMove = function(playerMove) {
+  let result = "";
+  const computerMove = getComputerMove();
+  
   // if both move are the same
   if (playerMove === "rock") {
-    if (computerMove === "rock") {
-      result = "Tie";
-      moves.innerHTML = `
-      <span>You <img src="imgs/rock.jpg"/></span>
-      <span>Computer <img src="imgs/rock.jpg"/></span>
-      `;
+    if (computerMove === "scissors") {
+      result = "You win";
     }
     //if computer move is paper
     else if (computerMove === "paper") {
       result = "You Lose";
-      moves.innerHTML = `
-      <span>You <img src="imgs/rock.jpg"/></span>
-      <span>Computer <img src="imgs/paper.jpg"/></span>
-      `;
     }
     // if computer move is scissors
-    else if (computerMove === "scissors") {
-      result = "You Win";
-      moves.innerHTML = `
-      <span>You <img src="imgs/rock.jpg"/></span>
-      <span>Computer <img src="imgs/scissors.jpg"/></span>
-      `;
+    else if (computerMove === "rock") {
+      result = "Tie";
     }
   }
   // if both moves are the same
   else if (playerMove === "paper") {
-    if (computerMove === "paper") {
-      result = "Tie";
-      moves.innerHTML = `
-      <span>You <img src="imgs/paper.jpg"/></span>
-      <span>Computer <img src="imgs/paper.jpg"/></span>
-      `;
+    if (computerMove === "rock") {
+      result = "You win";
     }
     // if computer move is different
     else if (computerMove === "scissors") {
       result = "You Lose";
-      moves.innerHTML = `
-      <span>You <img src="imgs/paper.jpg"/></span>
-      <span>Computer <img src="imgs/scissors.jpg"/></span>
-      `;
-    } else if (computerMove === "rock") {
-      result = "You Win";
-      moves.innerHTML = `
-      <span>You <img src="imgs/paper.jpg"/></span>
-      <span>Computer <img src="imgs/rock.jpg"/></span>
-      `;
+    } else if (computerMove === "paper") {
+      result = "Tie";
     }
   }
   // if both moves are the same
   else if (playerMove === "scissors") {
-    if (computerMove === "scissors") {
-      result = "Tie";
-      moves.innerHTML = `
-      <span>You <img src="imgs/scissors.jpg"/></span>
-      <span>Computer <img src="imgs/scissors.jpg"/></span>
-      `;
+    if (computerMove === "paper") {
+      result = "You Win";
     }
     // if computer move is different
-    else if (computerMove === "paper") {
-      result = "You Win";
-      moves.innerHTML = `
-      <span>You <img src="imgs/scissors.jpg"/></span>
-      <span>Computer <img src="imgs/paper.jpg"/></span>
-      `;
+    else if (computerMove === "scissors") {
+      result = "Tie";
     } else if (computerMove === "rock") {
       result = "You Lose";
-      moves.innerHTML = `
-      <span>You <img src="imgs/scissors.jpg"/></span>
-      <span>Computer <img src="imgs/rock.jpg"/></span>
-      `;
     }
   }
-
+  
   if (result === "You Win") {
     score.wins += 1;
   } else if (result === "You Lose") {
@@ -128,10 +111,13 @@ const compareMove = function (playerMove) {
   } else if (result === "Tie") {
     score.ties += 1;
   }
-
+  
   localStorage.setItem("score", JSON.stringify(score));
-
-  resultEl.textContent = `${result}.`;
+  
+  resultEl.textContent = result;
+  moves.innerHTML = `You <img src="imgs/${playerMove}.png"/> 
+  <img src="imgs/${computerMove}.png"/> Computer
+  `
   scoreEl.innerHTML = `<span>Wins: ${score.wins}</span> -  <span>Loses: ${score.losses}</span> - <span>Ties: ${score.ties}</span>`;
 };
 
@@ -139,7 +125,7 @@ resetBtn.addEventListener("click", () => {
   score.wins = 0;
   score.losses = 0;
   score.ties = 0;
-
+  
   localStorage.removeItem("score");
   scoreEl.innerHTML = `<span>Wins: ${score.wins}</span> -  <span>Loses: ${score.losses}</span> - <span>Ties: ${score.ties}</span>`;
 });
